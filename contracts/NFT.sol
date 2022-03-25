6e5d674e82079013ac68146ef197a98dd9da53f7
kkamalvanshi <kkamalvanshi@gmail.com>
2022-03-24 20:59:52 -0400

    added contract and new deploy script

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,45 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-// some contents of contracts are taken from buildspace.so nft project
-pragma solidity ^0.8.0;
-//import openzeppelin contracts
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-import "hardhat/console.sol";
+contract NFT is ERC721URIStorage{
 
-contract NFT is ERC721URIStorage {
-
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
-    constructor() ERC721 ("NFT Collection Name", "SHAPE") {
-        console.log("Constructing Smart Contract");
+    uint public tokenCount; //if variable is not initialized within solidity, then value will set get set to its default type depending on the datatype (uint)
+    
+    constructor() ERC721("Square NFT", "Square"){
+        
     }
 
-    //Contents of Contract...
+    function mint(string memory _tokenURI) external returns(uint) {
+        tokenCount++; //incrementing token number since we're minting a new nft
+        _safeMint(msg.sender, tokenCount); //safemint reverts if given tokenID already exists
+        _setTokenURI(tokenCount, _tokenURI); //sets metadata for newly minted nft
 
-    function createNFT() public {
-        //current token id, initially at 0
-        uint256 newItemId = _tokenIds.current();
-
-        //mint nft sender to msg
-        _safeMint(msg.sender, newItemId);
-
-        //setting the data of nft
-        //second parameter stores the json url of the Token (tokenURI)
-
-        //go to jsonkeeper and enter in json url the format OpenSea uses
-        /*
-        {
-            "name": "Starship Bot",
-            "description": "A silent hero.",
-            "image": "https://i.imgur.com/Tu3o8Ve.mp4"
-        }
-        */
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/937C");
-
-        //incrementing id of the nft
-        _tokenIds.increment();
+        return(tokenCount);
     }
 }
