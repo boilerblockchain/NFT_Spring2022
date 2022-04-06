1a7d8b06e113bc32aee8e206a71df6d951799742
Jason Bodzy <jasonbodzy@gmail.com>
2022-04-05 23:03:29 -0400

    Wallet page implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
-import Login from './Login'
+import WalletPage from './WalletPage'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <WalletPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
